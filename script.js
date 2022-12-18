3279ad29a2c508eebbf4191a1513fb127687706c
nguHelon <nguhelon@gmail.com>
2022-12-18 09:45:28 +0100

    finished the onload JS functionality, which is to load the popular items"

---
+++ script.js
@@ -0,0 +1,52 @@
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const selectInput = document.getElementById("selectInput");
+const itemsDiv = document.querySelector(".items-div");
+
+//www.themealdb.com/api/json/v1/1/list.php?i=list
+
+window.addEventListener("DOMContentLoaded", () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=beef')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            data.meals.forEach((meal) => {
+                fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.idMeal}`)
+                    .then(response => response.json())
+                    .then((data) => {
+                        let item = document.createElement("div");
+                        item.classList.add("item");
+                        item.setAttribute("data-id", `${data.meals[0].idMeal}`)
+                        let content = "";
+                        content = `                            
+                            <div class="img">
+                                <img src="${data.meals[0].strMealThumb}" alt="">
+                            </div>
+                            <div class="item-info">
+                                <h2>${data.meals[0].strMeal}</h2>
+                                <p>
+                                    ${data.meals[0].strInstructions.substring(0, 90) + ". . ."}
+                                </p>
+                            </div>
+                            <div class="buttons">
+                                <a href="
+                                    ${data.meals[0].strYoutube}
+                                ">
+                                    <button><i class="fa-solid fa-play"></i> play Video</button>
+                                </a>
+                                <a href="">
+                                    <button>view recipe</button>
+                                </a>
+                            </div>
+                        `;
+                        item.innerHTML = content;
+                        itemsDiv.appendChild(item);
+                    })
+            })
+        })
+})
+
+selectInput.addEventListener("click", () => {
+    searchInput.setAttribute("placeholder", `search food by: ${selectInput.value}`);
+})
